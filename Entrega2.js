7d249e0d7d3b20385f89de9a2a3d3e116f290abb
ZHARTYL <joankmr07@gmail.com>
2024-08-08 21:57:22 -0500

    v0.0.1

---
+++ Entrega2.js
@@ -0,0 +1,4 @@
+// Proyecto: Lista de Tareas
+// Agregar Tareas: Permitir al usuario agregar nuevas tareas a la lista.
+// Cambiar estados: Permitir al usuario marcar tareas como  ,En proceso,completadas.
+// Eliminar Tareas: Permitir al usuario eliminar tareas de la lista.
