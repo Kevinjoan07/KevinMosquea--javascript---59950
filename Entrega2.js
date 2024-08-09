68f790a84eef69a0e91d3f3fa971ad98c49ebb44
ZHARTYL <joankmr07@gmail.com>
2024-08-08 22:31:59 -0500

    V0.0.1

--- Entrega2.js
+++ Entrega2.js
@@ -1,1 +1,5 @@
-alert("Hola mundo")
+// Proyecto: Lista de Tareas
+
+// Agregar Tareas: Permitir al usuario agregar nuevas tareas a la lista.
+// actualizar estado de la tarea: Permitir al usuario marcar tareas como  Activo,vencidos , completadas.
+// Eliminar Tareas: Permitir al usuario eliminar tareas de la lista.
